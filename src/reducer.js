5cae6598fbdf7be12ae79cabed45ed1b5f02b34e
RichCode <chineduogada@gmail.com>
2021-02-06 08:42:24 +0100

    feature(reduce:done with implementation

---
+++ src/reducer.js
@@ -0,0 +1,24 @@
+//State []
+
+let lastId = 0;
+
+// A Reducer must a new state
+export default function reducer(state = [], action) {
+  switch (action.type) {
+    case "bugAdded":
+      return [
+        ...state,
+        {
+          id: ++lastId,
+          description: action.payload.description,
+          resolved: false,
+        },
+      ];
+
+    case "bugRemoved":
+      return state.filter((bug) => bug.id !== action.payload.id);
+
+    default:
+      return state;
+  }
+}
